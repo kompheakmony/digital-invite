00a03bf37e9328b8a0c84bbfe0cdef75277fd881
Nyz Cam <70509420+kompheakmony@users.noreply.github.com>
2025-10-14 05:01:06 +0000

    improve pattern performent

--- my-card/src/components/PatternBackground.tsx
+++ my-card/src/components/PatternBackground.tsx
@@ -1,13 +1,14 @@
 import React, { memo } from "react";
+import holKhmerPattern from "/hol-khmer.svg";
 
 const PatternBackground = memo(() => {
-  return (
-    <div 
-      className="pattern-bg"
-    />
-  );
+  const style: React.CSSProperties = {
+    ["--bg-url" as any]: `url(${holKhmerPattern})`,
+    ["--tile-size" as any]: "100px",
+  };
+
+  return <div className="pattern-bg" style={style} />;
 });
 
-PatternBackground.displayName = 'PatternBackground';
-
+PatternBackground.displayName = "PatternBackground";
 export default PatternBackground;

9241eb01aa6c3a6058e93e97c3940601eda9180d
Kumpheakmny <70509420+kompheakmony@users.noreply.github.com>
2025-11-12 04:55:07 +0000

    70

--- my-card/src/components/LayoutWrapper.tsx
+++ my-card/src/components/LayoutWrapper.tsx
@@ -174,7 +174,7 @@ const LayoutWrapper: FC<LayoutWrapperProps> = ({ children }) => {
         />
         <TopLeft color={currentTheme.accent} />
         <TopRight color={currentTheme.accent} />
-        <div className="relative w-full h-[calc(100vh-16rem)] sm:h-[calc(100vh-12rem)] lg:h-[calc(100vh-8rem)]">
+        <div className="relative w-full h-[92vh] sm:h-[70vh] md:h-[90vh] lg:h-[92vh]">
           <motion.ul
             ref={ref}
             className="h-full list-none overflow-y-scroll space-y-4 scrollbar-hide"
